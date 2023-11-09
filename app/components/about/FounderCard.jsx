f54a1a1ad0a38df37e460a44e34f6015d4dbbd3e
DeeFromNextDoor <simeonvictordayo@gmail.com>
2023-11-09 07:18:10 +0100

    little changes

--- app/components/about/FounderCard.jsx
+++ app/components/about/FounderCard.jsx
@@ -1,54 +1,73 @@
 "use client";
 import { useEffect, useState } from "react";
 import { founders } from "@/app/constant/data";
+import Image from "next/image";
 
 export default function FounderCard() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const numVisibleCards = 3;
 
-  const nextSlide = () => {
-    setCurrentIndex((currentIndex + 1) % founders.length);
-  };
-
   useEffect(() => {
-    const intervalId = setInterval(nextSlide, 5000);
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % founders.length);
+    }, 5000);
+
     return () => clearInterval(intervalId);
-  }, [currentIndex]);
+  }, [founders.length]);
 
   return (
-    <div className="overflow-hidden">
-      <div
-        className="flex transition-transform duration-1000 ease-in-out"
-        style={{
-          width: `${founders.length * (100 / numVisibleCards)}%`,
-          transform: `translateX(-${
-            (currentIndex % founders.length) * (100 / numVisibleCards)
-          }%)`,
-        }}
-      >
-        {founders.map((founder, index) => (
-          <div
+    <div className="relative">
+      <div className="overflow-hidden">
+        <div
+          className="flex transition-transform duration-1000 ease-in-out"
+          style={{
+            width: `${founders.length * (100 / numVisibleCards)}%`,
+            transform: `translateX(-${
+              currentIndex * (100 / founders.length)
+            }%)`,
+          }}
+        >
+          {founders.map((founder, index) => (
+            <div
+              key={index}
+              className="flex flex-col bg-gray-200 rounded-lg p-4 m-4 gap-4"
+              style={{ width: `${100 / numVisibleCards}%` }}
+            >
+              <div
+                className="relative"
+                style={{ width: "100%", height: "0", paddingBottom: "100%" }}
+              >
+                <Image
+                  src={founder.image}
+                  alt={founder.name}
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
+                  priority
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-2xl font-medium">{founder.name}</h1>
+                <small>{founder.role}</small>
+                <ul className="flex gap-4">
+                  {founder.icons.map((icon, idx) => (
+                    <li key={idx}>{icon}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 flex justify-center p-4">
+        {founders.map((_, index) => (
+          <span
             key={index}
-            className="flex flex-col bg-gray-200 rounded-lg p-4 m-4 gap-4"
-            style={{ width: `${100 / numVisibleCards}%` }}
-          >
-            <div className="bg-gray rounded-lg overflow-hidden">
-              <img
-                src={founder.image}
-                alt={founder.name}
-                className="w-full h-auto object-cover"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <h1 className="text-2xl font-medium">{founder.name}</h1>
-              <small>{founder.role}</small>
-              <ul className="flex gap-4">
-                {founder.icons.map((icon, idx) => (
-                  <li key={idx}>{icon}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
+            className={`inline-block h-3 w-3 mx-1 rounded-full ${
+              currentIndex === index ? "bg-red" : "bg-deep-gray"
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          />
         ))}
       </div>
     </div>
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,35 +1,47 @@
 import Link from "next/link";
+import photo from "../../public/images/signup-login.png";
+import Image from "next/image";
 
 export default function Login() {
   return (
-    <main className="min-h-screen min-w-screen flex items-center gap-4 justify-center p-4">
-      <div className="hidden md:block w-3/5">
-        <img src="/images/dl.beatsnoop 1.png" alt="Logo" className=" h-auto" />
-      </div>
-      <div className="w-full md:w-2/5">
-        <div className="max-w-md mx-auto flex flex-col gap-4">
-          <h1 className="text-2xl font-bold mb-2">Log in to Exclusive</h1>
-          <p className="text-sm mb-4">Enter your details below</p>
-          <form className="flex flex-col space-y-4">
-            <input
-              type="text"
-              placeholder="Email or Phone Number"
-              className="border-b border-gray-400 focus:outline-none focus:border-black"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="border-b border-gray-400 focus:outline-none focus:border-black"
-            />
-            <div className="flex items-center justify-between">
-              <button className="btn-primary">Log In</button>
-              <Link href="/forgot" className="text-sm text-red">
-                Forgot Password?
-              </Link>
-            </div>
-          </form>
+    <section className="mt-20 lg:mt-0 py-8">
+      <div className="flex flex-col gap-4 md:flex-row items-center p-2">
+        <div className="flex-1 h-[600px] w-full relative">
+          <Image
+            className="object-cover"
+            src={photo}
+            alt="Signup Logo"
+            quality={100}
+            priority
+            layout="fill"
+          />
+        </div>
+
+        <div className="w-full md:w-2/5">
+          <div className="max-w-md mx-auto flex flex-col gap-4">
+            <h1 className="text-2xl font-bold mb-2">Log in to Exclusive</h1>
+            <p className="text-sm mb-4">Enter your details below</p>
+            <form className="flex flex-col space-y-4">
+              <input
+                type="text"
+                placeholder="Email or Phone Number"
+                className="border-b border-gray-400 focus:outline-none focus:border-black"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="border-b border-gray-400 focus:outline-none focus:border-black"
+              />
+              <div className="flex items-center justify-between">
+                <button className="btn-primary">Log In</button>
+                <Link href="/forgot" className="text-sm text-red">
+                  Forgot Password?
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </main>
+    </section>
   );
 }
